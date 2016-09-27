bc13f3185eb58ac19eb7a51c36ea5d20122f53aa
marcSLSD <marc.salsedo78@gmail.com>
2016-09-27 14:36:03 +0200

    Add main class

---
+++ src/tdd0/Main.java
@@ -0,0 +1,11 @@
+package tdd0;
+
+/**
+ * Projet pooaTDD
+ * Created by masalsed on 27/09/2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Test Driven Development");
+    }
+}
